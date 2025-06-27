c4817aaacb9864b75c6e6ce4a11bac5d90010bfa
Abhinav Sharma <abhinav21ai@gmail.com>
2025-06-27 19:00:11 +0530

    Updated homepage

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="bg-white shadow p-4 flex items-center relative">
-      <div className="font-bold text-xl absolute left-8">Burrow</div>
+      <div style={{ fontFamily: '"Baloo 2", cursive' }} className="font-bold text-xl absolute left-8">Burrow</div>
       <div className="flex gap-16 mx-auto">
         <Link to="/home" className="hover:text-blue-600">Home</Link>
         <Link to="/chatbot" className="hover:text-blue-600">Chat</Link>
