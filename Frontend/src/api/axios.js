d17f46ce1cf874f6e6d00d1dc3a715e30e37eb63
AdarshKumarSr <adarshsrivastava824@gmail.com>
2025-12-24 19:21:51 +0530

    fix axios error

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,7 +2,20 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  withCredentials: true, // VERY IMPORTANT
 });
+
+// ✅ ADD THIS (CRITICAL)
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
